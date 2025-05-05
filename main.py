f8e5b3ce156cf8300ea47c27a0daca93ed399f47
robertopfneto <neto2014roberto@gmail.com>
2025-05-05 15:38:14 -0300

    json response setup

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ from flask import Flask, make_response, jsonify, request
 from bd import Carros
 # Instanciando flask
 app = Flask(__name__)
-
+app.config['JSON_SORT_KEYS'] = False
 @app.route('/carros', methods=['GET'])
 def get_carros():
     return make_response(
-        jsonify(Carros) #Retornando a tabela carros 
+        jsonify(
+            message='Lista de carros.',
+            data = Carros
+            ) #Retornando a tabela carros 
     )
     
 @app.route('/carros', methods=['POST'])
 def create_carro():
     carro = request.json #faz uma requisicao solicitando o bojeto
     Carros.append(carro) # adiciono na lista o carro
-    return carro
+    return make_response(
+        jsonify(
+            message='Carro cadastrado com sucesso!',
+            data=carro
+        )
+    )
 
 
 app.run()
